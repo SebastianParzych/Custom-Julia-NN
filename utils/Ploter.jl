edb3f4b898b65abeb42af7d4eff5bef744a74e94
Parzych <parzysebo@gmail.com>
2022-06-29 11:37:21 +0200

    ipynb update

--- utils/Ploter.jl
+++ utils/Ploter.jl
@@ -14,5 +14,5 @@ function tripleMetricPlot(Loss_history, acc_train, acc_test)
     p1 = plot(Loss_history, xlabel="Iteration", ylabel="Loss function", label="")
     p2 = plot(acc_train, xlabel="Iteration", ylabel="Training accuracy", label="")
     p3 = plot(acc_test, xlabel="Iteration", ylabel="Test accuracy", label="", ylim=(-0.01, 1.01))
-    plot(p1, p2, p3, layout=(3, 1))
+    display(plot(p1, p2, p3, layout=(3, 1)))
 end
